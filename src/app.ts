6e9a9c5814ae02b6b9e2515398608ff8f7dfb3bf
Женя Короленко <zenakorolenko@MacBook-Pro-iMag-2.local>
2023-06-04 17:18:54 +0300

    add eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -9,32 +9,32 @@ import { TYPES } from './types';
 import { UserController } from './users/users.controller';
 
 @injectable()
- export class App {
-  app: Express;
-  server: Server;
-  port: number;
+export class App {
+	app: Express;
+	server: Server;
+	port: number;
 
-  constructor(
-    @inject(TYPES.ILogger) private logger: ILogger, 
-    @inject(TYPES.UserController) private userController: UserController, 
-    @inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter, 
-  ) {
-    this.app = express();
-    this.port = 8000;
-  }
+	constructor(
+		@inject(TYPES.ILogger) private logger: ILogger,
+		@inject(TYPES.UserController) private userController: UserController,
+		@inject(TYPES.ExceptionFilter) private exceptionFilter: ExceptionFilter,
+	) {
+		this.app = express();
+		this.port = 8000;
+	}
 
-  useRoutes() {
-    this.app.use('/users', this.userController.router);
-  }
+	useRoutes(): void {
+		this.app.use('/users', this.userController.router);
+	}
 
-  useExceptionFilters() {
-    this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter))
-  }
+	useExceptionFilters(): void {
+		this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+	}
 
-  public async init() {
-    this.useRoutes();
-    this.useExceptionFilters();
-    this.server = this.app.listen(this.port);
-    this.logger.log([`server is running on port: ${this.port}`]);
-  }
- }
+	public async init(): Promise<void> {
+		this.useRoutes();
+		this.useExceptionFilters();
+		this.server = this.app.listen(this.port);
+		this.logger.log([`server is running on port: ${this.port}`]);
+	}
+}
