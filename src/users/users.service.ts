7b66780674ead0abbad158db46bb1264d70562c9
Женя Короленко <zenakorolenko@MBP-iMag-2.lan>
2023-06-19 00:01:46 +0300

    guard middleware was added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -39,4 +39,8 @@ export class UserService implements IUserService {
 
     return newUser.comparePassword(password);
   }
+
+  async getUserInfo(email: string): Promise<UserModel | null> {
+    return this.usersRepository.find(email);
+  }
 }
